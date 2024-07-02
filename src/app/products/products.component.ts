72b8def2b8ad7b9eff33ff45ca962e47df5b2e86
IMLV058\Biznes03 <hieunv@biznes.vn>
2024-07-02 16:34:12 +0700

    ngIf/ng-template

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,10 +9,40 @@ export class ProductsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-  products: { id: number; name: string; price: number; source: string }[] = [
-    { id: 1, name: 'MacBook Pro 16', source: '/assets/mac.jpg', price: 3000 },
-    { id: 2, name: 'MacBook Pro 15', source: '/assets/mac.jpg', price: 900 },
-    { id: 3, name: 'MacBook Pro 13', source: '/assets/mac.jpg', price: 1500 },
-    { id: 4, name: 'MacBook Pro 14', source: '/assets/mac.jpg', price: 2000 },
+  products: {
+    id: number;
+    name: string;
+    price: number;
+    source: string;
+    available: String;
+  }[] = [
+    {
+      id: 1,
+      name: 'MacBook Pro 16',
+      source: '/assets/mac.jpg',
+      price: 3000,
+      available: 'Available',
+    },
+    {
+      id: 2,
+      name: 'MacBook Pro 15',
+      source: '/assets/mac.jpg',
+      price: 900,
+      available: 'Not Available',
+    },
+    {
+      id: 3,
+      name: 'MacBook Pro 13',
+      source: '/assets/mac.jpg',
+      price: 1500,
+      available: 'Not Available',
+    },
+    {
+      id: 4,
+      name: 'MacBook Pro 14',
+      source: '/assets/mac.jpg',
+      price: 2000,
+      available: 'Available',
+    },
   ];
 }
